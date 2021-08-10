b1e88de96f084c5d1713da543aa81de4ba10b1f6
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-08-10 15:05:34 +0700

    add commander

--- bin/page-loader.ts
+++ bin/page-loader.ts
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 
-import { program } from 'commander'
+import { Command } from 'commander'
+
+const CWD = process.cwd()
+const program = new Command()
 
 program
     .version('0.0.1')
     .description('Page loader utility')
+    .option('-o, --output [dir]', 'output dir', CWD)
+
+program
+    .argument('url')
+    .action((url, options) => {
+        console.log('url', url)
+        console.log('options', options);
+
+    })
 
 program.parse(process.argv);
