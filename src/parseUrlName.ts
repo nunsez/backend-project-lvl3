41537aaede8add7fb351c41141c114ff830305ea
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-08-10 18:11:35 +0700

    add url name parser

--- src/parseUrlName.ts
+++ src/parseUrlName.ts
@@ -1,5 +1,14 @@
 const parseUrlName = (str: string) => {
-    return str
+    const delimiter = '-'
+    const decoded = decodeURI(str)
+
+    const pureStr = decoded
+        .replace(/^https?:\/\//, '')
+        .replace(/.html$/, '')
+
+    const result = pureStr.split(/_|[^\w]/).filter(Boolean)
+
+    return result.join(delimiter).concat('.html')
 }
 
 export default parseUrlName
